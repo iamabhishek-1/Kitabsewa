17ce15b1808cc68391bd84e97e9e7db5d0095609
Abhishek Kushwaha <rohanoffical12@gmail.com>
2025-12-06 20:29:43 +0545

    Update settings.py

--- projectKetabsewa/settings.py
+++ projectKetabsewa/settings.py
@@ -119,13 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
